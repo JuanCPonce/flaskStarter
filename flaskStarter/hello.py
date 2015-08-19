e8db7c9a1209310ea4557abba2ce2b9ba3dfaa96
JuanCPonce <Cielo@Yuyuhakusho.local>
2015-08-18 22:11:46 -0700

    Added comments to hello.py file as I need to investigate why templates not rendering

--- flaskStarter/hello.py
+++ flaskStarter/hello.py
@@ -16,6 +16,8 @@ def show_user_profile(username):
 
 @app.route('/hello/<name>')
 def hello(name=None):
+	# This needs to be looked into as it dosen't make sense that this renders.
+	# I'm likely not following the correct process and will have to check on it.
     return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
